5ec2e7c4054d5038a6ade6d08d43cba7fcabd397
ZoreX <aydinhsimli@gmail.com>
2022-12-09 00:24:14 +0400

    Add project files.

---
+++ WPFGalleryMVVM/Model/Images.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFGalleryMVVM.Model
+{
+    public class Images : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null!)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged!;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        private string price;
+
+        public string Price
+        {
+            get { return price; }
+            set { price = value; OnPropertyChanged(); }
+        }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; OnPropertyChanged(); }
+        }
+
+        private string imagePath;
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+
+        private string nationality;
+
+        public string Nationality
+        {
+            get { return nationality; }
+            set { nationality = value; }
+        }
+
+        private string descripton;
+
+        public string Description
+        {
+            get { return descripton; }
+            set { descripton = value; OnPropertyChanged(); }
+        }
+
+
+
+
+    }
+}
---
+++ WPFGalleryMVVM/Repositories/FakeRepo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFGalleryMVVM.Model;
+
+namespace WPFGalleryMVVM.Repositories
+{
+    public class FakeRepo
+    {
+        public static ObservableCollection<Images> GetImages()
+        {
+            return new ObservableCollection<Images>()
+            {
+                new Images()
+                {
+                    Name="Cristiano Ronaldo",
+                    ImagePath="../../../Image/ronaldo.jpg",
+                    Nationality="Portugal",
+                    Price="500000000$",
+                    Date=new DateTime(1985,05,05),
+                    Description="Suuuuuuuiiii"
+                },
+                new Images()
+                {
+                    Name="Lionel Messi",
+                    ImagePath="../../../Image/messi.jpg",
+                    Nationality="Argentina",
+                    Price="470000000$",
+                    Date=new DateTime(1987,09,22),
+                    Description="Suuuuuuuiiii"
+                },
+                new Images()
+                {
+                    Name="Fernando Maradona",
+                    ImagePath="../../../Image/maradona.jpg",
+                    Nationality="Argentina",
+                    Price="3800000000$",
+                    Date=new DateTime(1992,05,05),
+                    Description="Suuuuuuuiiii"
+                },
+                new Images()
+                {
+                    Name="Oliver Kahn",
+                    ImagePath="../../../Image/kahn.jpg",
+                    Nationality="Almanya",
+                    Price="2700000000$",
+                    Date=new DateTime(1969,07,15),
+                    Description="Suuuuuuuiiii"
+                },
+                new Images()
+                {
+                    Name="Pele",
+                    ImagePath="../../../Image/pele.jpg",
+                    Nationality="Brazil",
+                    Price="3500000000$",
+                    Date=new DateTime(1940,05,25),
+                    Description="Suuuuuuuiiii"
+                },
+                new Images()
+                {
+                    Name="Johan Cruyff",
+                    ImagePath="../../../Image/cruyff.jpg",
+                    Nationality="Holland",
+                    Price="1900000000$",
+                    Date=new DateTime(1947,09,25),
+                    Description="Suuuuuuuiiii"
+                },
+                new Images()
+                {
+                    Name="Carles Puyol",
+                    ImagePath="../../../Image/puyol.jpg",
+                    Nationality="Spain",
+                    Price="3800000000$",
+                    Date=new DateTime(1978,11,14),
+                    Description="Suuuuuuuiiii"
+                }
+
+            };
+        }
+    }
+}
