8f6ee0d26c1bcf3c0a460e93114178d1471ce76d
ZoreX <aydinhsimli@gmail.com>
2022-12-09 23:13:52 +0400

    done

--- WPFGalleryMVVM/ViewModel/MainViewModel.cs
+++ WPFGalleryMVVM/ViewModel/MainViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFGalleryMVVM.Model;
 
 namespace WPFGalleryMVVM.ViewModel
 {
     public class MainViewModel:BaseViewModel
     {
+        public List<Images> Imagess { get; set; }
 
+
+        public MainViewModel()
+        {
+
+        }
     }
 }
