f701fa91ae90331567a766a1e78bec53f54e0157
Aydin Hesimli <aydinhsimli@gmail.com>
2022-12-21 12:34:48 +0400

    done

--- WPFGalleryMVVM/ViewModel/MainViewModel.cs
+++ WPFGalleryMVVM/ViewModel/MainViewModel.cs
@@ -13,54 +13,58 @@ using WPFGalleryMVVM.Windows;
 
 namespace WPFGalleryMVVM.ViewModel
 {
-    public class MainViewModel:BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
-        
 
-        public Images image { get;set;}
+
+        private Images selectedItem;
+
+        public Images SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value; OnPropertyChanged(); }
+        }
+
         public int index { get; set; }
 
-        public ObservableCollection<Images> Imagess { get; set; }
+        private ObservableCollection<Images> images;
 
-       
+        public ObservableCollection<Images> Imagess
+        {
+            get { return images; }
+            set { images = value; OnPropertyChanged(); }
+        }
 
-        public RelayCommand DoubleClickCommand { get; set; }
+
+
+
         public RelayCommand MediumCommand { get; set; }
         public RelayCommand SmallCommand { get; set; }
         public RelayCommand LargeCommand { get; set; }
         public RelayCommand AboutCommand { get; set; }
         public RelayCommand ExitCommand { get; set; }
+        public RelayCommand SelectImageCommand { get; set; }
         public MainViewModel()
         {
+            SelectedItem = new Images();
             Imagess = new ObservableCollection<Images>();
-            Imagess= new ObservableCollection<Images>(Repositories.FakeRepo.GetImages());
+            Imagess = new ObservableCollection<Images>(Repositories.FakeRepo.GetImages());
 
-            DoubleClickCommand = new RelayCommand((d) =>
+
+            SelectImageCommand = new RelayCommand((o) =>
             {
-                var ListBox = d as ListBox;
-
-                if (ListBox.SelectedItem is Images item)
-                {
-                    SecondPage secondPage = new SecondPage();
-                    
-                    //secondPage.compyImage = Imagess;
-
-
-
-                }
-
+                MessageBox.Show(SelectedItem.Name);
             });
-
             MediumCommand = new RelayCommand((o) =>
             {
-                var w= o as Window;
-              w.Resources["ButtonFS"] = 150.0;
+                var w = o as Window;
+                w.Resources["ButtonFS"] = 150.0;
             });
 
             SmallCommand = new RelayCommand((o) =>
             {
                 var w = o as Window;
-               w.Resources["ButtonFS"] = 50.0;
+                w.Resources["ButtonFS"] = 50.0;
             });
 
             LargeCommand = new RelayCommand((o) =>
